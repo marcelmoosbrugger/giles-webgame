c511257af2fb2e5ef897a90c74c284c22b10ac03
Marcel Moosbrugger <marcelmoosbrugger@gmail.com>
2017-09-12 12:28:24 +0200

    Wrote about page

--- src/app/components/presentationals/AboutPage.jsx
+++ src/app/components/presentationals/AboutPage.jsx
@@ -18,13 +18,79 @@ export default class AboutPage extends React.Component {
         return (
             <div className="about-page">
                 <h1>About Giles's Game</h1>
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh.</p>
-                <h2>Lorem ipsum</h2>
-                <p>Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi. Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada. Praesent congue erat at massa. Sed cursus turpis vitae tortor.</p>
-                <p>Donec posuere vulputate arcu. Phasellus accumsan cursus velit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam, nisi quis porttitor congue, elit erat euismod orci, ac placerat dolor lectus quis orci. Phasellus consectetuer vestibulum elit. Aenean tellus metus, bibendum sed, posuere ac, mattis non, nunc. Vestibulum fringilla pede sit amet augue. In turpis. Pellentesque posuere. Praesent turpis. Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis lacus. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Nullam sagittis. Suspendisse pulvinar, augue ac venenatis condimentum, sem libero volutpat nibh, nec pellentesque velit pede quis nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce id purus. Ut varius tincidunt libero. Phasellus dolor. Maecenas vestibulum mollis diam. Pellentesque ut neque. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In dui magna, posuere eget, vestibulum et, tempor auctor, justo. In ac felis quis tortor malesuada pretium. Pellentesque auctor neque nec urna. Proin sapien ipsum, porta a, auctor quis, euismod ut, mi.</p>
-                <p>Aenean viverra rhoncus pede. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Ut non enim eleifend felis pretium feugiat. Vivamus quis mi. Phasellus a est. Phasellus magna. In hac habitasse platea dictumst. Curabitur at lacus ac velit ornare lobortis. Curabitur a felis in nunc fringilla tristique. Morbi mattis ullamcorper velit. Phasellus gravida semper nisi. Nullam vel sem. Pellentesque libero tortor, tincidunt et, tincidunt eget, semper nec, quam. Sed hendrerit. Morbi ac felis. Nunc egestas, augue at pellentesque laoreet, felis eros vehicula leo, at malesuada velit leo quis pede. Donec interdum, metus et hendrerit aliquet, dolor diam sagittis ligula, eget egestas libero turpis vel mi. Nunc nulla. Fusce risus nisl, viverra et, tempor et, pretium in, sapien. Donec venenatis vulputate lorem. Morbi nec metus. Phasellus blandit leo ut odio. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Sed magna purus, fermentum eu, tincidunt eu, varius ut, felis. In auctor lobortis lacus. Quisque libero metus, condimentum nec, tempor a, commodo mollis, magna. Vestibulum ullamcorper mauris at ligula. Fusce fermentum. Nullam cursus lacinia erat. Praesent blandit laoreet nibh.</p>
-                <p>Fusce convallis metus id felis luctus adipiscing. Pellentesque egestas, neque sit amet convallis pulvinar, justo nulla eleifend augue, ac auctor orci leo non est. Quisque id mi. Ut tincidunt tincidunt erat. Etiam feugiat lorem non metus. Vestibulum dapibus nunc ac augue. Curabitur vestibulum aliquam leo. Praesent egestas neque eu enim. In hac habitasse platea dictumst. Fusce a quam. Etiam ut purus mattis mauris sodales aliquam. Curabitur nisi. Quisque malesuada placerat nisl. Nam ipsum risus, rutrum vitae, vestibulum eu, molestie vel, lacus. Sed augue ipsum, egestas nec, vestibulum et, malesuada adipiscing, dui. Vestibulum facilisis, purus nec pulvinar iaculis, ligula mi congue nunc, vitae euismod ligula urna in dolor. Mauris sollicitudin fermentum libero. Praesent nonummy mi in odio. Nunc interdum lacus sit amet orci. Vestibulum rutrum, mi nec elementum vehicula, eros quam gravida nisl, id fringilla neque ante vel mi. Morbi mollis tellus ac sapien. Phasellus volutpat, metus eget egestas mollis, lacus lacus blandit dui, id egestas quam mauris ut lacus. Fusce vel dui. Sed in libero ut nibh placerat accumsan. Proin faucibus arcu quis ante. In consectetuer turpis ut velit. Nulla sit amet est. Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus.</p>
-                <p>Cras risus ipsum, faucibus ut, ullamcorper id, varius ac, leo. Suspendisse feugiat. Suspendisse enim turpis, dictum sed, iaculis a, condimentum nec, nisi. Praesent nec nisl a purus blandit viverra. Praesent ac massa at ligula laoreet iaculis. Nulla neque dolor, sagittis eget, iaculis quis, molestie non, velit. Mauris turpis nunc, blandit et, volutpat molestie, porta ut, ligula. Fusce pharetra convallis urna. Quisque ut nisi. Donec mi odio, faucibus at, scelerisque quis, convallis in, nisi. Suspendisse non nisl sit amet velit hendrerit rutrum. Ut leo. Ut a nisl id ante tempus hendrerit. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Suspendisse eu ligula. Nulla facilisi. Donec id justo. Praesent porttitor, nulla vitae posuere iaculis, arcu nisl dignissim dolor, a pretium mi sem ut ipsum. Curabitur suscipit suscipit tellus. Praesent vestibulum dapibus nibh. Etiam iaculis nunc ac metus. Ut id nisl quis enim dignissim sagittis. Etiam sollicitudin, ipsum eu pulvinar rutrum, tellus ipsum laoreet sapien, quis venenatis ante odio sit amet eros. Proin magna. Duis vel nibh at velit scelerisque suscipit. Curabitur turpis. Vestibulum suscipit nulla quis orci. Fusce ac felis sit amet ligula pharetra condimentum.</p>
+                <p>
+                    Giles's Game is a logical game between two players.
+                    Let's call them David and Kurt.
+                    David and Kurt both own multiset of formulas, called the player's <em>tenet</em>.
+                    A single state in the game consists of the two tenets of the players.
+                    In literature a game state is denoted as
+                    <em> [&kappa;<sub>1</sub>, ..., &kappa;<sub>n</sub> | &delta;<sub>1</sub>, ..., &delta;<sub>m</sub>]</em>,
+                    where <em>[&kappa;<sub>1</sub>, ..., &kappa;<sub>n</sub>]</em> is the tenet of Kurt and
+                    <em> [&delta;<sub>1</sub>, ..., &delta;<sub>m</sub>]</em> is the tenet of David.
+                    Although a general game state is allowed to contain multiple formulas, a Giles's game
+                    always starts with the state <em>[ | &phi;]</em>.
+                    Before the game starts, an interpretation for &phi; has to be given.
+                    Obeying the rules described shortly, such a game is called <em>Giles's game for a formula &phi;</em>.
+                    The game ends if a state is reached where all formulas in both tenets are atomic.
+                </p>
+                <h2>Rules</h2>
+                <p>
+                    As mentioned, the game starts in a state [ | &phi;], for a formula &phi;.
+                    A general state, however, looks like [&kappa;<sub>1</sub>, ..., &kappa;<sub>n</sub> | &delta;<sub>1</sub>, ..., &delta;<sub>m</sub>].
+                    The description of the rules will be given for the general case, which also applies in the beginning of a game.
+                </p>
+                <p>
+                    So let's assume that the game is in a state [&kappa;<sub>1</sub>, ..., &kappa;<sub>n</sub> | &delta;<sub>1</sub>, ..., &delta;<sub>m</sub>].
+                    If all formulas &kappa;<sub>1</sub>, ..., &kappa;<sub>n</sub> and &delta;<sub>1</sub>, ..., &delta;<sub>m</sub> are atomic, the game ends.
+                    The payoffs for David and Kurt are then determined as described in the next section.
+                </p>
+                <p>
+                    If the game is not in a final state, a non atomic formula is picked from either the tenet of David or the tenet of Kurt.
+                    The player from who's tenet the current formula was picked, gets the role of the <em>proponent</em>, the other player acts as the <em>opponent</em>.
+                    Based on the outer connective of the current formula, one of the following rules applies and the game continues in the resulting state:
+                </p>
+                <ul>
+                    <li>
+                        If the current formula is <em>&alpha;&and;&beta;</em>, then the opponent chooses either <em>&alpha;</em> or <em>&beta; </em>
+                        to replace <em>&alpha;&and;&beta;</em> in the proponent's tenet with.
+                    </li>
+                    <li>
+                        If the current formula is <em>&alpha;&or;&beta;</em>, then the proponent chooses either <em>&alpha;</em> or <em>&beta; </em>
+                        to replace <em>&alpha;&or;&beta;</em> in the proponent's tenet with.
+                    </li>
+                    <li>
+                        If the current formula is <em>&alpha;&rarr;&beta;</em>, the opponent gets to choose whether to continue the game in
+                        a state where the current formula is removed from the proponent's tenet or in a state where
+                        the current formula is replaced by <em>&beta;</em> in the proponent's tenet and <em>&alpha;</em> is added to the opponent's tenet.
+                    </li>
+                    <li>
+                        If the current formula is <em>&forall;x&alpha;(x)</em>, then it gets replaced by <em>&alpha;(c)</em>, where the domain
+                        element <em>c</em> is chosen by the opponent.
+                    </li>
+                    <li>
+                        If the current formula is <em>&exist;x&alpha;(x)</em>, then it gets replaced by <em>&alpha;(c)</em>, where the domain
+                        element <em>c</em> is chosen by the proponent.
+                    </li>
+                    <li>
+                        If the current formula is <em>&alpha;&amp;&beta;</em>, then by the choice of the proponent, <em>&alpha;&amp;&beta;</em> gets either replaced
+                        by both <em>&alpha;</em> and <em>&beta;</em> or by <em>&perp;</em>.
+                    </li>
+                </ul>
+                <p>
+                    An explicit rule for the negation can be omitted as <em>&not;&alpha;</em> gets translated to <em>&alpha;&rarr;&perp;</em>.
+                </p>
+                <h2>Final state</h2>
+                <p>
+                    If a state is reached were all formulae in both tenets are atomic, the game ends and the
+                    two players have to pay money to each other. The amount of money Kurt has to give
+                    to David, depends on the atomic formulae in Kurt’s tenet. For every atomic formula &kappa;
+                    in Kurt’s final tenet, a binary experiment is executed which succeeds with probability
+                    <em> e<sub>M</sub>(&kappa;)</em> and fails with probability <em>1 − e<sub>M</sub>(&kappa;)</em>,
+                    for an interpretation <em>M</em>. This is the reason why an interpretation has to be given before the game starts.
+                    A failed experiment results in Kurt owing David 1 &euro;. By analogy, the same procedure applies to David.
+                    The risk associated with a single atomic formula &alpha; can be denoted as <em>&lt;&alpha;&gt; </em>
+                    and is equal to  <em>1 − e<sub>M</sub>(&alpha;)</em> (the probability that the experiment corresponding to &alpha; fails).
+                </p>
             </div>
         );
     }
